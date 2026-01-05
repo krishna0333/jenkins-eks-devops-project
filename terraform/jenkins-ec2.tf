19a26982dd2799ab176d0d8c6d412eb0f109851d
murali <murali@gmail.com>
2026-01-05 12:02:13 +0530

    hj

--- terraform/jenkins-ec2.tf
+++ terraform/jenkins-ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "jenkins" {
   user_data = <<-EOF
     #!/bin/bash
     yum update -y
-    yum install docker -y
+    
     systemctl start docker
     systemctl enable docker
 
